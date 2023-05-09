321088f2fda1f13511347f013b9ccb60c1b27b10
Bárbara Martins <martins.barbara04@gmail.com>
2023-05-09 20:24:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+
+def main(argv):
+    operation, input_file, output_file = get_arguments(argv)
+
+    if output_file:
+        file_name = output_file
+    else:
+        file_name = input_file.replace('.txt', '.z78')
+
+    # if operation == '-c':
+    #     compress
+    # elif operation == '-x':
+    #     decompress
+
+
+def get_arguments(argv):
+    operation = ""
+    input_file = ""
+    output_file = ""
+
+    if len(argv) >= 2:
+        operation = argv[0]
+        input_file = argv[1]
+        if len(argv) == 4:
+            output_file = argv[3]
+    else:
+        print('Invalid arguments')
+        print(
+            "main.py <OPERATION: -c COMPRESS || -x DECOMPRESS> <INPUT FILE> [-o OUTPUT FILE]"
+        )
+        os._exit(1)
+
+    return operation, input_file, output_file
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
