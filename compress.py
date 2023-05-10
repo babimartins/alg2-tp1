bbcab1af8f389e5f4daa72d1289ce8a7ec8c7678
Bárbara Martins <martins.barbara04@gmail.com>
2023-05-09 21:52:12 -0300

    Update compress.py

--- compress.py
+++ compress.py
@@ -6,10 +6,26 @@ BIG = 'big'
 CHAR_SIZE = 1
 
 
-def reduce_function(accumulators: tuple[Trie, bytes, str], char):
-    dictionary = accumulators[0]
-    output = accumulators[1]
-    string = accumulators[2]
+def compress(input_file, output_file):
+    input = ""
+    with open(input_file, 'r', encoding="utf-8") as file:
+        input = file.read()
+
+    init = [Trie(), bytes(), '']
+    dictionary, output, string = reduce(__reduce_function, input, init)
+
+    code = dictionary.find(string)
+    code_size = ceil(code.bit_length() / 8.0)
+    output += code_size.to_bytes(1, BIG) + code.to_bytes(code_size, BIG)
+
+    with open(output_file, 'wb') as file:
+        file.write(output)
+
+
+def __reduce_function(tuples: tuple[Trie, bytes, str], char):
+    dictionary = tuples[0]
+    output = tuples[1]
+    string = tuples[2]
 
     if dictionary.find(string + char) > -1:
         string = string + char
@@ -26,19 +42,3 @@ def reduce_function(accumulators: tuple[Trie, bytes, str], char):
         string = ''
 
     return (dictionary, output, string)
-
-
-def compress(input_file, output_file):
-    input = ""
-    with open(input_file, 'r', encoding="utf-8") as file:
-        input = file.read()
-
-    dictionary, output, string = reduce(reduce_function, input,
-                                        (Trie(), bytes(), ''))
-
-    code = dictionary.find(string)
-    code_size = ceil(code.bit_length() / 8.0)
-    output += code_size.to_bytes(1, BIG) + code.to_bytes(code_size, BIG)
-
-    with open(output_file, 'wb') as file:
-        file.write(output)
