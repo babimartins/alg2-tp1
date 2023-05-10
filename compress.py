54f4849d61488f81f1b126efbea87c0578f447ca
Bárbara Martins <martins.barbara04@gmail.com>
2023-05-09 22:04:31 -0300

    Update compress.py

--- compress.py
+++ compress.py
@@ -14,9 +14,9 @@ def compress(input_file, output_file):
     init = [Trie(), bytes(), '']
     dictionary, output, string = reduce(__reduce_function, input, init)
 
-    code = dictionary.find(string)
-    code_size = ceil(code.bit_length() / 8.0)
-    output += code_size.to_bytes(1, BIG) + code.to_bytes(code_size, BIG)
+    index = dictionary.find(string)
+    index_size = ceil(index.bit_length() / 8.0)
+    output += index_size.to_bytes(1, BIG) + index.to_bytes(index_size, BIG)
 
     with open(output_file, 'wb') as file:
         file.write(output)
@@ -31,12 +31,12 @@ def __reduce_function(tuples: tuple[Trie, bytes, str], char):
         string = string + char
 
     else:
-        code = dictionary.find(string)
-        code_size = ceil(code.bit_length() / 8.0)
+        index = dictionary.find(string)
+        index_size = ceil(index.bit_length() / 8.0)
         num_char = ord(char)
 
-        output += code_size.to_bytes(1, BIG) + code.to_bytes(
-            code_size, BIG) + num_char.to_bytes(CHAR_SIZE, BIG)
+        output += index_size.to_bytes(1, BIG) + index.to_bytes(
+            index_size, BIG) + num_char.to_bytes(CHAR_SIZE, BIG)
 
         dictionary.insert(string + char)
         string = ''
